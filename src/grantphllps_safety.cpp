eb0b0059cea41ae74afec4aabd8ad8d3c851ad82
Grant Phillips <grantphllps@gmail.com>
2023-03-07 12:07:56 -0800

    grants first time

---
+++ src/grantphllps_safety.cpp
@@ -0,0 +1,63 @@
+#include "ros/ros.h"
+#include "std_msgs/Bool.h"
+#include "ackermann_msgs/AckermannDriveStamped.h"
+#include "sensor_msgs/LaserScan.h"
+#include "nav_msgs/Odometry.h"
+#include <iostream>
+
+//Global Variables
+ros::Publisher brake_pub, brake_bool_pub;
+
+std_msgs::Bool brake_bool_msg;
+ackermann_msgs::AckermannDriveStamped brake_msg;
+
+float angleIncrement = 0.00582315586507;    //Radians, from the /scan rostopic
+float* vx = new float;                      //Pointer to the current speed variable
+float ttcThreshold = 0.320;                 //Minimum time-to-collision before applying the brake
+float zero = 0.0;                           //Just a 0,0 as a float
+
+void scanCallback(const sensor_msgs::LaserScan::ConstPtr& msg) {
+    //std::cout << msg->ranges[269] << std::endl;
+    float rDot;
+    float angle;
+    float tcc;
+    float v = *vx;
+    for (int i = 0; i < msg->ranges.size(); i++) {
+        //For each beam, calculate its angle, and the velocity the car is moving in that direction
+        angle = i * angleIncrement;
+        rDot = -1.0*v*cos(angle);
+        rDot = std::max(rDot,zero);
+        //Calculate the time-to-collision in this direction
+        tcc = msg->ranges[i] / rDot;
+        //Filter out the -Inf's
+        if (tcc < 0)
+            tcc = 100000;
+        //Check if any of the ttc is less than some acceptable threshold
+        if (tcc < ttcThreshold) {
+            //If the ttc is < threshold, apply the brake
+            brake_pub.publish(brake_msg);
+            brake_bool_msg.data = true;
+            brake_bool_pub.publish(brake_bool_msg);
+        }
+    }
+}
+
+void odometryCallback(const nav_msgs::Odometry::ConstPtr& msg) {
+    //This function updates the global velocity value
+    *vx = msg->twist.twist.linear.x;
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "scan_answer");
+  ros::NodeHandle n;
+
+  ros::Subscriber scan_sub = n.subscribe("/scan", 1, scanCallback);
+  ros::Subscriber odom_sub = n.subscribe("/odom", 1, odometryCallback);
+  brake_pub = n.advertise<ackermann_msgs::AckermannDriveStamped>("/brake",1);
+  brake_bool_pub = n.advertise<std_msgs::Bool>("/brake_bool",1);
+
+  ros::spin();
+
+  return 0;
+}
